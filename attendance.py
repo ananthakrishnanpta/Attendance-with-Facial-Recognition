484364023bb84d5944667f7199aacb8b2b152eb9
ananthakrishnanpta <ananthakrishnan@techis.io>
2022-07-15 07:49:55 +0530

    got traversing images working. Still issues with the image lists. Might have to try with single images first

--- attendance.py
+++ attendance.py
@@ -1,16 +1,41 @@
 import cv2
 from cv2 import imshow
 import face_recognition as fr
-
+import glob # for the file traversing
 import os
 pathlib = 'dataset'
 images = []
 Names = []
+image_Dict = {}
 myList = os.listdir(pathlib)
 print(myList)
 for cl in myList:
-    currImg = cv2.imread(f'{pathlib}/{cl}')
+    currImg = cv2.imread(f'{pathlib}/{cl}/')
+    images = [cv2.imread(file) for file in glob.glob(f'{pathlib}/{cl}/*.jpg')]
+    # print(f'{pathlib}/{cl}')
     images.append(currImg)
     Names.append(os.path.splitext(cl)[0])
-    #print(Names)
-    # print(images)
+    print(Names)
+    print(images)
+    image_Dict[Names] = images
+
+print(image_Dict)
+    # cv2.imshow('img',currImg)
+
+#########################################
+# for cl in enumerate(myList):
+#     for image in cl:
+#         # currImg = cv2.imread(f'{pathlib}/{cl}/')
+#         cLocation = f'{pathlib}/{cl}/{image}'
+#         # print(cLocation)
+
+#         currImg = cv2.imread(f'cLocation')
+
+        
+#         images.append(currImg)
+#         Names.append(os.path.splitext(cl)[0])
+#         # print(Names)
+#         # print(images)
+#         # cv2.imshow('img',currImg)
+
+#########################################
